aa1df57ccc06f4d3bd97420cf7f002f4fa1589bf
Seven <seven@apowo.com>
2021-10-27 17:20:57 +0800

    init

---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+    networks:{
+        development: {
+            host: "127.0.0.1",     // Localhost (default: none)
+            port: 7545,            // Standard Ethereum port (default: none)
+            network_id: "5777",    // Any network (default: none)
+            gasPrice: 5000000000,  // 20 gwei (in wei) (default: 100 gwei)
+        },
+        live: {
+            provider: () => new HDWalletProvider([process.env.WALLET_PRIVATE_KEY], `https://mainnet.infura.io/v3/${process.env.WEB3_INFURA_PROJECT_ID}`, 0, 1),
+            network_id: 1,       // Mainnet's id
+            gas: 50000,        // Rinkeby has a lower block limit than mainnet
+            gasPrice: 90000000000,  // 20 gwei (in wei) (default: 100 gwei)
+            confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 500,  // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        },
+    },
+    // Configure your compilers
+    compilers: {
+        solc: {
+            version: "0.8.3",    // Fetch exact version from solc-bin (default: truffle's version)
+            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false
+        }
+    },
+}
